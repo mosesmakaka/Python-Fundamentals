f95095ec15977626f6f62f0d7a0a39282a2d250b
Moses Makaka <moses@MosesPC>
2023-02-15 18:27:59 +0300

    Student marks

---
+++ marks.py
@@ -0,0 +1,7 @@
+name = input("Enter student name here")
+maths = input("What did you get in Maths?")
+english = input("What did you get in English?")
+spanish = input("What did you get in Spanish?")
+physics = input("What did you get in Physics?")
+total = int(maths) + int(english) + int(spanish) + int(physics)
+print(total)
