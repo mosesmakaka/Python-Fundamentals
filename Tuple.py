b1b20269b37e681177f4eea86c3d614bec6360d6
Moses Makaka <moseskax@gmail.com>
2023-06-05 20:03:42 +0300

    Updates

--- Tuple.py
+++ Tuple.py
@@ -30,3 +30,27 @@ print(thistuple)
 
 thistuple = ("apple", "banana", "cherry")
 print(len(thistuple))  # Length of tuple or number of items in tuple.
+
+thistuple = ("apple",) # Tuple with one item. Don't forget the comma!
+print(type(thistuple))
+
+#NOT a tuple. This is a string.
+thistuple = ("apple")
+print(type(thistuple))
+
+tuple1 = ("apple", "banana", "cherry")
+tuple2 = (1, 5, 7, 9, 3)
+tuple3 = (True, False, False)
+
+tuple1 = ("abc", 34, True, 40, "male") # A tuple can contain different data types
+
+mytuple = ("apple", "banana", "cherry")
+print(type(mytuple))
+
+#Using the tuple() method to make a tuple:
+thistuple = tuple(("apple", "banana", "cherry")) # note the double round-brackets
+print(thistuple)
+
+#Print the second item in the tuple:
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[1])
