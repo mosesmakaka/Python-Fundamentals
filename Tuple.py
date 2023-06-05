0d8211728169ec04ab9cf3436254d6c336acb441
Moses Makaka <moseskax@gmail.com>
2023-06-05 19:58:12 +0300

    Updates

--- Tuple.py
+++ Tuple.py
@@ -24,3 +24,9 @@ Since tuples are indexed, they can have items with the same value:
 """
 thistuple = ("apple", "banana", "cherry")
 print(thistuple)
+
+thistuple = ("apple", "banana", "cherry", "apple", "cherry") # Allows duplicates
+print(thistuple)
+
+thistuple = ("apple", "banana", "cherry")
+print(len(thistuple))  # Length of tuple or number of items in tuple.
